afa6522d407d818be9e0c07e1146087ce2a3388b
sklohrm <sklohrm@gmail.com>
2025-10-15 13:14:51 -0500

    Updated OrderJdbcRepo

--- summative-assessment-java-databases-starter/src/main/java/org/example/data/impl/OrderJdbcRepo.java
+++ summative-assessment-java-databases-starter/src/main/java/org/example/data/impl/OrderJdbcRepo.java
@@ -4,9 +4,11 @@ import org.example.data.OrderRepo;
 import org.example.data.exceptions.InternalErrorException;
 import org.example.data.exceptions.RecordNotFoundException;
 import org.example.model.Order;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class OrderJdbcRepo implements OrderRepo {
     @Override
     public Order getOrderById(int id) throws RecordNotFoundException, InternalErrorException {
